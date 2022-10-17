c646989526a93702a815f3fe0477d97206e9494d
FIXED-TERM RUFINO JOYCE (MFC1-LA) <fixed-term.joyce.rufino@SEG-automotive.com>
2022-10-17 11:04:24 -0300

    Bloqueando páginas que precisam de autenticação

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
 import './App.css';
 import {BrowserRouter, Routes, Route, Navigate} from 'react-router-dom';
 
+//função que mapeia se a autentificação do usuario foi feita com sucesso
+import { onAuthStateChanged } from 'firebase/auth';
+
+//hooks
+import { useState, useEffect } from 'react';
+import { useAuthentication } from './hooks/useAuthentication';
+
 //context
 import { AuthProvider } from './context/AuthContext'
 
@@ -9,23 +16,53 @@ import Home from './pages/Home/Home';
 import About from './pages/About/About';
 import Login from './pages/Login/Login';
 import Register from './pages/Register/Register';
+import Dashboard from './pages/Dashboard/Dashboard';
+import CreatePost from './pages/CreatePost/CreatePost';
 
 //components
 import Navbar from './components/Navbar/Navbar';
 import Footer from './components/Footer/Footer';
 
 function App() {
+
+  const [user, setUser] = useState(undefined)
+  const {auth} = useAuthentication()
+
+  const loadingUser = user === undefined;
+
+  useEffect(() => {
+
+    onAuthStateChanged(auth, (user) => {
+      setUser(user);
+    })
+  }, [auth]);
+
+  if(loadingUser) {
+    return <p>Carregando......</p>
+  }
+
+
   return (
     <div className="App">
-      <AuthProvider>
+      <AuthProvider value={{ user }}>
         <BrowserRouter>
         <Navbar />
           <div className='container'>
             <Routes>
               <Route path='/' element={<Home />} />
               <Route path='/about' element={<About />} />
-              <Route path='/login' element={<Login />} />
-              <Route path='/register' element={<Register />} />
+              <Route  
+                path='/login' 
+                element={!user ? <Login /> : <Navigate to='/' />} />
+              <Route  
+                path='/register' 
+                element={!user ? <Register /> : <Navigate to='/' /> } />
+                <Route 
+                  path='/dashboard' 
+                  element={user ? <Dashboard /> : <Navigate to='/login' />  }  />
+              <Route  
+                  path='/post/create' 
+                  element={user ? <CreatePost /> : <Navigate to='/login' /> } />
             </Routes>
           </div>
           <Footer />
--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -1,10 +1,15 @@
+import { Link } from 'react-router-dom'
+
 //CSS
 import styles from './About.module.css'
 
+
 const About = () => {
   return (
-    <div>
-        <h1>About</h1>
+    <div className={styles.about}>
+        <h1>Sobre o Mini <span>Blog</span> </h1>
+        <p>Este projeto consiste em um blog, feito com ReactJS no front-end e, Firebase no back-end.</p>
+        <Link to='/post/create' className='btn'>Criar post.</Link>
     </div>
   )
 }
